6388b4c29388318deac8dc2ffd581d69a738ab2d
Pawan Singh <pmarsingh@gmail.com>
2021-09-03 23:11:02 +0530

    Initia

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log("Running");
+});
